3aeff5ddc78277e351db76f05f3df4726d17e9d1
kumarsaket601 <46194436+kumarsaket601@users.noreply.github.com>
2021-07-12 13:07:15 +0530

    js input

---
+++ app.js
@@ -0,0 +1,9 @@
+var btnTransalate = document.querySelector("#btn-transalate");
+var log = console.log;
+var
+
+btnTransalate.addEventListener("click" , clickHandler());
+
+function clickHandler(){
+   log("clicked!");
+}
