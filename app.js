64606212bda38ed673072754e05a1b48a233bdd3
kumarsaket601 <46194436+kumarsaket601@users.noreply.github.com>
2021-07-14 12:46:52 +0530

    js event

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 var btnTransalate = document.querySelector("#btn-transalate");
 var log = console.log;
-var
+var text = document.querySelector("#text-input");
+var outputTranslate = document.querySelector("#output"); 
+ 
+var serverUrl = "https://api.funtranslations.com/translate/yoda.json";
 
-btnTransalate.addEventListener("click" , clickHandler());
+function getTranslationUrl(text){
 
-function clickHandler(){
+   return serverUrl + "?" + "text" + text
+}
+
+
+
+  function clickHandler(){
+
+
+   var inputText = txtInput.value;
+
+   fetch(getTranslationUrl(text))
+     .then(response => response.json())
+     .then(json => log(json))
+
    log("clicked!");
 }
+
+btnTransalate.addEventListener("click" , clickHandler())
